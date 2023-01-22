1fa5246bb8d7a815c5b17ef45202ab0a463a6dbd
tfurmston <tfurmston@googlemail.com>
2023-01-22 21:03:25 +0000

    use local mode for tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,8 +3,8 @@ from pytest import fixture
 
 
 @fixture(scope='session')
-def test_ray_cluster(num_cpus: int = 4):
+def test_ray_cluster(num_cpus: int = 4, local_mode: bool = True):
     """Pytest fixture for construct a test Ray cluster."""
-    ray.init(num_cpus=num_cpus)
+    ray.init(num_cpus=num_cpus, local_mode=local_mode)
     yield
     ray.shutdown()
