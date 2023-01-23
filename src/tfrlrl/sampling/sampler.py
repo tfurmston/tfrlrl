9f9844099e242d1609fad9f182b36c03a15fb226
tfurmston <tfurmston@googlemail.com>
2023-01-23 21:56:50 +0000

    add unique ID for samples

--- src/tfrlrl/sampling/sampler.py
+++ src/tfrlrl/sampling/sampler.py
@@ -1,3 +1,4 @@
+import uuid
 from typing import Dict, Union
 
 import gym
@@ -23,8 +24,10 @@ class Sampler:
         limit on the number of sampled steps.
         """
         self._env = gym.make(env_id)
+        self._env_id = str(uuid.uuid4())
         self._n_steps = n_steps
         self._n_steps_taken = 0
+        self._n_env_steps_taken = 0
         self._observation = None
         self._next_observation = None
         self._action = None
@@ -36,20 +39,32 @@ class Sampler:
         """Ensure that the Sampler class supports the iterable protocol."""
         return self
 
-    def __next__(self) -> (NDArray, Union[int, float, NDArray], NDArray, float, bool, Dict):
+    def __next__(self) -> (str, int, NDArray, Union[int, float, NDArray], NDArray, float, bool, Dict):
         """Return the next item in the sampler iterator. If this is not possible, raise a StopIteration exception."""
         if self._n_steps is not None and self._n_steps_taken >= self._n_steps:
             raise StopIteration
 
         if self._done:
             self._observation = self._env.reset()
+            self._env_id = str(uuid.uuid4())
+            self._n_env_steps_taken = 0
         else:
             self._observation = self._next_observation
 
         self._action = self._env.action_space.sample()
         self._next_observation, self._reward, self._done, self._info = self._env.step(self._action)
         self._n_steps_taken += 1
-        return self._observation, self._action, self._next_observation, self._reward, self._done, self._info
+        self._n_env_steps_taken += 1
+        return (
+            self._env_id,
+            self._n_env_steps_taken,
+            self._observation,
+            self._action,
+            self._next_observation,
+            self._reward,
+            self._done,
+            self._info,
+        )
 
 
 class RaySampler:
@@ -74,6 +89,6 @@ class RaySampler:
         """Ensure that the RaySampler class supports the iterable protocol."""
         return self
 
-    def __next__(self) -> (NDArray, Union[int, float, NDArray], NDArray, float, bool, Dict):
+    def __next__(self) -> (str, int, NDArray, Union[int, float, NDArray], NDArray, float, bool, Dict):
         """Return the next item in the sampler iterator. If this is not possible, raise a StopIteration exception."""
         return ray.get([env.__next__.remote() for env in self._envs])
