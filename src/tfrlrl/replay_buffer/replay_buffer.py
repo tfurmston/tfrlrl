5d55adce22384fd12783a8378618c276ef1cec66
tfurmston <tfurmston@googlemail.com>
2023-02-26 10:22:26 +0000

    add sampling from replay buffer

--- src/tfrlrl/replay_buffer/replay_buffer.py
+++ src/tfrlrl/replay_buffer/replay_buffer.py
@@ -3,9 +3,16 @@ from typing import List
 
 import numpy as np
 
+from tfrlrl.data_models.replay_buffer import ReplayBufferSample
 from tfrlrl.data_models.step import Step, Steps
 
 logger = logging.getLogger(__name__)
+
+
+class ReplayBufferException(Exception):
+    """Custom exception to encompass errors raised by the replay buffer class."""
+
+    pass
 
 
 class ReplayBuffer:
@@ -30,13 +37,27 @@ class ReplayBuffer:
         )
 
         self._indx = 0
+        self._buffer_full = False
         self._buffer_size = buffer_size
 
     def _increment_index(self, sample_size):
+        if self._indx + sample_size >= self._buffer_size:
+            self._buffer_full = True
         self._indx = (self._indx + sample_size) % self._buffer_size
 
     def _calculate_sample_indices(self, sample_size) -> List[int]:
         return [x % self._buffer_size for x in range(self._indx, self._indx + sample_size)]
+
+    def _calculate_sample_range(self, n_steps: int) -> int:
+        if self._buffer_full:
+            indx = self._buffer_size
+        else:
+            indx = self._indx
+
+        if indx < n_steps:
+            raise ReplayBufferException(
+                'There are insufficient samples in the replay buffer to sample %s steps', n_steps)
+        return indx
 
     def _update_buffer(self, inds, observations, next_observations, actions, rewards, dones):
         self._observations[:, inds] = observations
@@ -71,6 +92,13 @@ class ReplayBuffer:
         )
         self._increment_index(steps.n_steps)
 
-    def sample(self, n_steps: int):
+    def sample(self, n_steps: int) -> ReplayBufferSample:
         """Sample the given number of steps from the replay buffer."""
-        raise NotImplementedError()
+        inds = np.random.permutation(self._calculate_sample_range(n_steps))[:n_steps]
+        return ReplayBufferSample(
+            observations=self._observations[:, inds],
+            next_observations=self._next_observations[:, inds],
+            actions=self._actions[:, inds],
+            rewards=self._rewards[:, inds],
+            dones=self._dones[:, inds],
+        )
