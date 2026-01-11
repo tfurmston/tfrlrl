4cecad1cce918a1457f12c939f6dd10f570e10d5
tfurmston <tfurmston@users.noreply.github.com>
2026-01-11 15:26:17 +0000

    Add tests for RayEpisodicSampler class

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,8 +3,8 @@ from pytest import fixture
 
 
 @fixture(scope='session')
-def test_ray_cluster(num_cpus: int = 4, local_mode: bool = True):
+def test_ray_cluster(num_cpus: int = 2):
     """Pytest fixture for construct a test Ray cluster."""
-    ray.init(num_cpus=num_cpus, local_mode=local_mode)
+    ray.init(num_cpus=num_cpus)
     yield
     ray.shutdown()
--- tests/tfrlrl/sampling/test_episodic_sampler.py
+++ tests/tfrlrl/sampling/test_episodic_sampler.py
@@ -11,18 +11,21 @@ from hypothesis import strategies as st
 from tfrlrl.features.onehot import construct_one_hot_feature_function
 from tfrlrl.policies.base import BasePolicy
 from tfrlrl.policies.linear_soft_max import LinearSoftMax
-from tfrlrl.sampling.episodic_sampler import EpisodicSampler
+from tfrlrl.sampling.episodic_sampler import (
+    EpisodicSampler,
+    RayEpisodicSampler,
+)
 from tfrlrl.sampling.statistics_collection import BaseStatisticsCollector
 
 
 @dataclass
-class TestStatistics:
+class DummyStatistics:
     """Dataclass for the statistics collected test sample collections."""
 
     samples: list
 
 
-class TestStatisticsCollector(BaseStatisticsCollector):
+class DummyStatisticsCollector(BaseStatisticsCollector):
     """Test class for collecting statistics during sampling."""
 
     def __init__(self):
@@ -43,12 +46,14 @@ class TestStatisticsCollector(BaseStatisticsCollector):
 
     def aggregate_statistics(self):
         """Aggregate the statistics collected by the collector."""
-        return TestStatistics(samples=self._samples)
+        return DummyStatistics(samples=self._samples)
 
     @classmethod
-    def merge_statistics(cls, samples: List[TestStatistics]):
+    def merge_statistics(cls, statistics: List[DummyStatistics]):
         """Aggregate the statistics collected by the collector."""
-        return list(chain.from_iterable(samples))
+        return DummyStatistics(
+            samples=list(chain.from_iterable([x.samples for x in statistics])),
+        )
 
 
 class TestEpisodicSampler:
@@ -75,7 +80,7 @@ class TestEpisodicSampler:
             softmax_parameters,
             feature_fn,
         )
-        stats_collector = TestStatisticsCollector()
+        stats_collector = DummyStatisticsCollector()
         sampler = EpisodicSampler(env_id, stats_collector, n_episodes=n_episodes, policy=pol)
         for statistics in sampler:
             assert isinstance(statistics.samples, list)
@@ -110,7 +115,7 @@ class TestEpisodicSampler:
             softmax_parameters,
             feature_fn,
         )
-        stats_collector = TestStatisticsCollector()
+        stats_collector = DummyStatisticsCollector()
         sampler = EpisodicSampler(
             env_id,
             stats_collector,
@@ -149,8 +154,165 @@ class TestEpisodicSampler:
             softmax_parameters,
             feature_fn,
         )
-        stats_collector = TestStatisticsCollector()
+        stats_collector = DummyStatisticsCollector()
         sampler = EpisodicSampler(env_id, stats_collector, n_episodes=n_episodes, policy=pol)
+
+        # First iteration: consume all episodes
+        first_iteration_count = 0
+        for statistics in sampler:
+            assert isinstance(statistics.samples, list)
+            for step in statistics.samples:
+                assert isinstance(step.env_id, str)
+                assert isinstance(step.time_step, int)
+                assert isinstance(step.observation, np.ndarray)
+                assert isinstance(step.next_observation, np.ndarray)
+                assert isinstance(step.reward, float) or isinstance(step.reward, int)
+                assert isinstance(step.done, bool)
+                assert isinstance(step.info, dict)
+            first_iteration_count += 1
+
+        assert first_iteration_count == n_episodes
+
+        # Iterator should be exhausted - trying to iterate should yield no results
+        exhausted_count = 0
+        for _, _ in sampler:
+            exhausted_count += 1
+
+        assert exhausted_count == 0
+
+        # Reset the sampler
+        sampler.reset()
+
+        # Second iteration: should work again after reset
+        second_iteration_count = 0
+        for statistics in sampler:
+            assert isinstance(statistics.samples, list)
+            for step in statistics.samples:
+                assert isinstance(step.env_id, str)
+                assert isinstance(step.time_step, int)
+                assert isinstance(step.observation, np.ndarray)
+                assert isinstance(step.next_observation, np.ndarray)
+                assert isinstance(step.reward, float) or isinstance(step.reward, int)
+                assert isinstance(step.done, bool)
+                assert isinstance(step.info, dict)
+            second_iteration_count += 1
+
+        assert second_iteration_count == n_episodes
+
+
+class TestRayEpisodicSampler:
+    """Class that encapsulates the unit tests for the RayEpisodicSampler class."""
+
+    @pytest.mark.parametrize('env_id', ['FrozenLake-v1'])
+    @given(n_episodes=st.integers(min_value=2, max_value=10))
+    @settings(deadline=2000)
+    def test_ray_sample_n_episodes_without_limit(self, env_id: str, n_episodes: int, test_ray_cluster):
+        """
+        Test that n-episodes can be sampled from the environment and that the outputs follow the expected format.
+
+        :param env_id: The Gym environment ID to be used in the sampling.
+        :param n_steps: The number of steps to sample from the environment.
+        :param test_ray_cluster: Test Ray cluster.
+        """
+        n_samplers = 2
+        env = gym.make(env_id)
+        S = env.observation_space.n
+        A = env.action_space.n
+
+        feature_fn = construct_one_hot_feature_function(S=S, A=A)
+        softmax_parameters = np.random.random(size=S * (A - 1))
+        pol = LinearSoftMax(
+            env_id,
+            softmax_parameters,
+            feature_fn,
+        )
+        stats_collector = DummyStatisticsCollector()
+        sampler = RayEpisodicSampler(n_samplers, env_id, stats_collector, n_episodes=n_episodes, policy=pol)
+        for statistics in sampler:
+            assert isinstance(statistics.samples, list)
+            for step in statistics.samples:
+                assert isinstance(step.env_id, str)
+                assert isinstance(step.time_step, int)
+                assert isinstance(step.observation, np.ndarray)
+                assert isinstance(step.next_observation, np.ndarray)
+                assert isinstance(step.reward, float) or isinstance(step.reward, int)
+                assert isinstance(step.done, bool)
+                assert isinstance(step.info, dict)
+
+    @given(n_episodes=st.integers(min_value=2, max_value=10))
+    @settings(deadline=2000)
+    def test_ray_sample_episode_with_env_kwargs(self, n_episodes: int, test_ray_cluster):
+        """
+        Test that environment kwargs are correctly passed through to the environment construction.
+
+        Uses FrozenLake-v1 with is_slippery parameter to verify kwargs functionality.
+
+        :param n_steps: The number of steps to sample from the environment.
+        :param test_ray_cluster: Test Ray cluster.
+        """
+        n_samplers = 2
+        env_id = 'FrozenLake-v1'
+        env = gym.make(env_id)
+        S = env.observation_space.n
+        A = env.action_space.n
+
+        feature_fn = construct_one_hot_feature_function(S=S, A=A)
+        softmax_parameters = np.random.random(size=S * (A - 1))
+        pol = LinearSoftMax(
+            env_id,
+            softmax_parameters,
+            feature_fn,
+        )
+        stats_collector = DummyStatisticsCollector()
+        sampler = RayEpisodicSampler(
+            n_samplers,
+            env_id,
+            stats_collector,
+            n_episodes=n_episodes,
+            policy=pol,
+            is_slippery=False,
+        )
+        for statistics in sampler:
+            assert isinstance(statistics.samples, list)
+            for step in statistics.samples:
+                assert isinstance(step.env_id, str)
+                assert isinstance(step.time_step, int)
+                assert isinstance(step.observation, np.ndarray)
+                assert isinstance(step.next_observation, np.ndarray)
+                assert isinstance(step.reward, float) or isinstance(step.reward, int)
+                assert isinstance(step.done, bool)
+                assert isinstance(step.info, dict)
+
+    @given(n_episodes=st.integers(min_value=2, max_value=10))
+    @settings(deadline=2000)
+    def test_ray_reset_allows_reuse_as_iterator(self, n_episodes: int, test_ray_cluster):
+        """
+        Test that the reset method allows the EpisodicSampler to be used as an iterator multiple times.
+
+        :param n_episodes: The number of episodes to sample from the environment.
+        :param test_ray_cluster: Test Ray cluster.
+        """
+        n_samplers = 2
+        env_id = 'FrozenLake-v1'
+        env = gym.make(env_id)
+        S = env.observation_space.n
+        A = env.action_space.n
+
+        feature_fn = construct_one_hot_feature_function(S=S, A=A)
+        softmax_parameters = np.random.random(size=S * (A - 1))
+        pol = LinearSoftMax(
+            env_id,
+            softmax_parameters,
+            feature_fn,
+        )
+        stats_collector = DummyStatisticsCollector()
+        sampler = RayEpisodicSampler(
+            n_samplers,
+            env_id,
+            stats_collector,
+            n_episodes=n_episodes,
+            policy=pol,
+        )
 
         # First iteration: consume all episodes
         first_iteration_count = 0
