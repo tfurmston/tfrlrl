1c34ff43c7187633bd8d1cb6918c6fdb47da03c3
tfurmston <tfurmston@users.noreply.github.com>
2026-01-11 15:43:25 +0000

    update test

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,10 +1,16 @@
-import ray
+import os
+
 from pytest import fixture
 
 
 @fixture(scope='session')
 def test_ray_cluster(num_cpus: int = 2):
     """Pytest fixture for construct a test Ray cluster."""
+    os.environ['RAY_ACCEL_ENV_VAR_OVERRIDE_ON_ZERO'] = '0'
+
+    import ray
+
     ray.init(num_cpus=num_cpus)
     yield
     ray.shutdown()
+    del os.environ['RAY_ACCEL_ENV_VAR_OVERRIDE_ON_ZERO']
