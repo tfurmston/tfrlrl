d318eca17a9a3c73f7900ac7f0a9dc1aed387ba4
tfurmston <tfurmston@users.noreply.github.com>
2026-01-31 12:46:02 +0000

    Update merge_statistics

--- src/tfrlrl/data_models/statistics.py
+++ src/tfrlrl/data_models/statistics.py
@@ -1,6 +1,12 @@
 from dataclasses import dataclass
 
 
+class StatisticsException(Exception):
+    """Custom exception to encompass errors raised during statistics collection."""
+
+    pass
+
+
 @dataclass
 class BaseStatistics:
     """A base class for statistics collected from a statistics collector."""
