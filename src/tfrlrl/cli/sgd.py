36bf7c1780a99771849d1f2c386e8139d6490e53
tfurmston <tfurmston@users.noreply.github.com>
2026-03-07 16:38:57 +0000

    Allow different policy classes in SGD CLI

--- src/tfrlrl/cli/sgd.py
+++ src/tfrlrl/cli/sgd.py
@@ -2,7 +2,11 @@ import argparse
 import json
 import logging
 
+import gymnasium as gym
+
+from tfrlrl.features.onehot import OneHotFeatureFunction
 from tfrlrl.policies.dense_neural_network import DenseNetworkPolicy
+from tfrlrl.policies.linear_soft_max import LinearSoftMax
 from tfrlrl.training_algorithms.sgd import train_policy_gradient
 
 logging.basicConfig(format='%(asctime)s %(message)s', force=True)
@@ -56,6 +60,20 @@ def parse_args(args=None):
         default='{}',
         help='Environment-specific keyword arguments as a JSON string (e.g., \'{"is_slippery": false}\').',
     )
+    parser.add_argument(
+        '--policy-class',
+        type=str,
+        required=True,
+        choices=['linear', 'dense'],
+        help='The type of policy class to use in the stochastic gradient algorithm.',
+    )
+    parser.add_argument(
+        '--n-hidden',
+        type=int,
+        nargs='+',
+        default=[16, 32],
+        help='The number of hidden dimensions to use in a dense policy network.',
+    )
     return parser.parse_args(args)
 
 
@@ -81,10 +99,17 @@ def main(args=None):
     if env_kwargs is not None:
         logger.info('Environment Arguments: %s', env_kwargs)
 
-    policy = DenseNetworkPolicy(
-        env_id=parsed_args.env_id,
-        hidden_space_dims=[16, 32],
-    )
+    if parsed_args.policy_class == 'linear':
+        logger.info('Using a linear policy with a one-hot feature encoding.')
+        env = gym.make(parsed_args.env_id)
+        feature_fn = OneHotFeatureFunction(env.observation_space.n, env.action_space.n)
+        policy = LinearSoftMax(parsed_args.env_id, feature_fn)
+    else:
+        logger.info('Using a dense policy with hidden dimensions: %s', parsed_args.n_hidden)
+        policy = DenseNetworkPolicy(
+            env_id=parsed_args.env_id,
+            hidden_space_dims=parsed_args.n_hidden,
+        )
 
     train_policy_gradient(
         env_id=parsed_args.env_id,
--- tests/tfrlrl/cli/test_sgd.py
+++ tests/tfrlrl/cli/test_sgd.py
@@ -14,17 +14,58 @@ class TestParseArgs:
         'args,expected',
         [
             (
-                ['--env-id', 'FrozenLake-v1', '--n-iterations', '10', '--n-episodes', '20', '--alpha', '1.0'],
-                {'env_id': 'FrozenLake-v1', 'n_iterations': 10, 'n_episodes': 20, 'alpha': 1.0, 'env_kwargs': '{}'},
+                [
+                    '--env-id',
+                    'FrozenLake-v1',
+                    '--n-iterations',
+                    '10',
+                    '--n-episodes',
+                    '20',
+                    '--alpha',
+                    '1.0',
+                    '--policy-class',
+                    'dense',
+                ],
+                {
+                    'env_id': 'FrozenLake-v1',
+                    'n_iterations': 10,
+                    'n_episodes': 20,
+                    'alpha': 1.0,
+                    'env_kwargs': '{}',
+                    'policy_class': 'dense',
+                },
             ),
             (
-                ['--env-id', 'FrozenLake-v1', '--env-kwargs', '{"is_slippery": false}'],
+                ['--env-id', 'FrozenLake-v1', '--env-kwargs', '{"is_slippery": false}', '--policy-class', 'dense'],
                 {
                     'env_id': 'FrozenLake-v1',
                     'n_iterations': 100,
                     'n_episodes': 100,
                     'alpha': 100.0,
                     'env_kwargs': '{"is_slippery": false}',
+                    'policy_class': 'dense',
+                },
+            ),
+            (
+                [
+                    '--env-id',
+                    'FrozenLake-v1',
+                    '--n-iterations',
+                    '10',
+                    '--n-episodes',
+                    '20',
+                    '--alpha',
+                    '1.0',
+                    '--policy-class',
+                    'linear',
+                ],
+                {
+                    'env_id': 'FrozenLake-v1',
+                    'n_iterations': 10,
+                    'n_episodes': 20,
+                    'alpha': 1.0,
+                    'env_kwargs': '{}',
+                    'policy_class': 'linear',
                 },
             ),
         ],
@@ -47,25 +88,44 @@ class TestParseArgs:
 class TestMain:
     """Tests for the main function."""
 
-    @pytest.mark.parametrize('env_id', ['InvertedPendulum-v5'])
+    @pytest.mark.parametrize(
+        'env_id, policy_class',
+        [
+            (
+                'InvertedPendulum-v5',
+                'dense',
+            ),
+            (
+                'FrozenLake-v1',
+                'linear',
+            ),
+        ],
+    )
     @given(
         n_iterations=st.integers(min_value=1, max_value=3),
         n_episodes=st.integers(min_value=5, max_value=15),
         alpha=st.floats(min_value=0.0001, max_value=0.001),
     )
     @settings(deadline=10000)
-    def test_main_with_different_parameters(self, env_id: str, n_iterations: int, n_episodes: int, alpha: float):
+    def test_main_with_different_parameters(
+        self, env_id: str, policy_class: str, n_iterations: int, n_episodes: int, alpha: float
+    ):
         """
         Test main function with different parameter values.
 
-        :param env_id: The Gym environment ID to be used in training.
-        :param n_iterations: The number of policy updates to perform.
-        :param n_episodes: The number of episodes to sample during each policy update.
-        :param alpha: The initial step size for stochastic gradient ascent.
+        Args:
+            env_id: The Gym environment ID to be used in training.
+            policy_class: The policy class to use in SGD.
+            n_iterations: The number of policy updates to perform.
+            n_episodes: The number of episodes to sample during each policy update.
+            alpha: The initial step size for stochastic gradient ascent.
+
         """
         args = [
             '--env-id',
             env_id,
+            '--policy-class',
+            policy_class,
             '--n-iterations',
             str(n_iterations),
             '--n-episodes',
@@ -78,16 +138,32 @@ class TestMain:
 
         assert exit_code is None or exit_code == 0
 
-    @pytest.mark.parametrize('env_id', ['InvertedPendulum-v5'])
-    def test_main_with_env_kwargs(self, env_id: str):
+    @pytest.mark.parametrize(
+        'env_id, policy_class, env_kwargs',
+        [
+            ('InvertedPendulum-v5', 'dense', '{"reset_noise_scale": 0.001}'),
+            (
+                'FrozenLake-v1',
+                'linear',
+                '{"is_slippery": false}',
+            ),
+        ],
+    )
+    def test_main_with_env_kwargs(self, env_id: str, policy_class: str, env_kwargs: str):
         """
         Test main function with env-kwargs set.
 
-        :param env_id: The Gym environment ID to be used in training.
+        Args:
+            env_id: The Gym environment ID to be used in training.
+            policy_class: The policy class to use in SGD.
+            env_kwargs: Any key-words passed to the environment.
+
         """
         args = [
             '--env-id',
             env_id,
+            '--policy-class',
+            policy_class,
             '--n-iterations',
             '2',
             '--n-episodes',
@@ -95,23 +171,40 @@ class TestMain:
             '--alpha',
             '1.0',
             '--env-kwargs',
-            '{"reset_noise_scale": 0.001}',
+            env_kwargs,
         ]
 
         exit_code = main(args)
 
         assert exit_code is None or exit_code == 0
 
-    @pytest.mark.parametrize('env_id', ['InvertedPendulum-v5'])
-    def test_main_with_default_env_kwargs(self, env_id: str):
+    @pytest.mark.parametrize(
+        'env_id, policy_class',
+        [
+            (
+                'InvertedPendulum-v5',
+                'dense',
+            ),
+            (
+                'FrozenLake-v1',
+                'linear',
+            ),
+        ],
+    )
+    def test_main_with_default_env_kwargs(self, env_id: str, policy_class: str):
         """
         Test main function with default (empty) env-kwargs.
 
-        :param env_id: The Gym environment ID to be used in training.
+        Args:
+            env_id: The Gym environment ID to be used in training.
+            policy_class: The policy class to use in SGD.
+
         """
         args = [
             '--env-id',
             env_id,
+            '--policy-class',
+            policy_class,
             '--n-iterations',
             '2',
             '--n-episodes',
@@ -140,7 +233,9 @@ class TestMain:
         """
         Test main function with malformed env-kwargs returns exit code 1.
 
-        :param malformed_json: Malformed JSON string to test.
+        Args:
+            malformed_json: Malformed JSON string to test.
+
         """
         args = [
             '--env-id',
@@ -153,6 +248,8 @@ class TestMain:
             '1.0',
             '--env-kwargs',
             malformed_json,
+            '--policy-class',
+            'dense',
         ]
 
         exit_code = main(args)
